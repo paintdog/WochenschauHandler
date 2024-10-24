b0e0562c7aaec7ea65b5e2d778aa7d018bed7e03
Paintdog <paintdog@users.noreply.github.com>
2024-10-24 23:54:12 +0200

    small changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # WochenschauHandler
- 
+
+Letztlich eine Fingerübung, um die Wochenschau-Reihe „Methoden historischen Lernens“ zu scrapen und im Anschluss für den [Wikipedia-Artikel](https://de.wikipedia.org/wiki/Wochenschau_Verlag#Buchreihe_%E2%80%9EMethoden_historischen_Lernens%E2%80%9C) aufzubereiten.
--- main.py
+++ main.py
@@ -4,16 +4,23 @@ from bs4 import BeautifulSoup
 url = "https://www.wochenschau-verlag.de/Reihe/Methoden-historischen-Lernens?order=name-asc&p={page}"
 
 
-for i in range(1, 2+1):
-    url = f"https://www.wochenschau-verlag.de/Reihe/Methoden-historischen-Lernens?order=name-asc&p={i}"
-    # print(url)
-    # Abruf der einzelnen Seite
+def download2soup(url):
     html = requests.get(url)
     with open("dump.html", "w", encoding="utf-8") as f:
         f.write(html.text)
     # print("Dump abgespeichert...")
     # html2soup
     soup = BeautifulSoup(html.text, "html5lib")
+    return soup
+    
+
+
+for i in range(1, 2+1):
+    url = f"https://www.wochenschau-verlag.de/Reihe/Methoden-historischen-Lernens?order=name-asc&p={i}"
+    
+    # print(url)
+    # Abruf der einzelnen Seite
+    soup = download2soup(url)
 
     # Produktinformationen ermitteln
     product_infos = soup.find_all('div', attrs={"class": "product-info"})
@@ -23,8 +30,10 @@ for i in range(1, 2+1):
          # print(item.text)
 
          # Produkttitel ermitteln und ausgeben
-         product_titles = item.find_all("a", attrs={"class": "product-name"})
-         for product_title in product_titles:
-             print("* ''" + product_title.text.strip("\t\n\r ") + "''.")
-
+         products = item.find_all("a", attrs={"class": "product-name"})
+         for product in products:
+             print(f"Handling: {product.text.strip("\t\n\r ")}...")
+             
+             print(f"URL: {product["href"]}\n")
+             
          
